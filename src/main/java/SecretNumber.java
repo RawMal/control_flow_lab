60f1f38aca06114b854fac5f3d3d9a71c8650d1e
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-11 16:13:06 +0100

    Create response if secret number is found

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class SecretNumber {
     public static void main(String[] args) {
 
 // Prompt user to guess the secret number
-
-// Create secret number variable
+        System.out.println("Can you guess the secret number?, type it in below!");
 
 // Collect user input/Create scanner object
+        Scanner reader = new Scanner(System.in);
+        String SecretNumber = reader.nextLine();
 
 // Create response for when secret number is found
 
