5c4f1b6bbb1f33d333849f912121543868b759ef
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-11 23:09:58 +0100

    what to wear start and prompt user

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class WhatToWear {
     public static void main(String[] args) {
+// Prompt user to ask what to wear
+        Scanner reader = new Scanner(System.in);
+        String weatherAdvice = reader.nextLine();
+// Collect user input/ create scanner object
 
+// Create ranges and responses for temperature
+
+// Create responses for weather conditions
     }
 }
