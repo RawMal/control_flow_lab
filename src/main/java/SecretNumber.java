0f5523eb5c6d757ee82a1090d8656db082ec2940
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-11 17:37:40 +0100

    Secret Number Game

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class SecretNumber {
     public static void main(String[] args) {
 
-// Prompt user to guess the secret number
-        System.out.println("Can you guess the secret number from 1 to 20?, type it in below!");
+//  Prompt user to guess the secret number
+        System.out.println("Can you guess the last number in the lock combination Sherlock?,It's from 1-20 type it in below!");
 
-// Collect user input/Create scanner object
+//  Collect user input/Create scanner object
         Scanner reader = new Scanner(System.in);
         int Secret_Number = Integer.parseInt(reader.nextLine());
 
-// Create response for when secret number is found
+//  Create response for when secret number is found
         if (Secret_Number==(13)){
             System.out.println("ELEMENTARY WATSON, YOU FOUND IT!!!");
-// Create ranges and response for wrong number
+//  Create ranges and response for wrong number
         } else if (Secret_Number < 13){
             System.out.println("Not quite old boy, you fell too short!");
+        } else if (Secret_Number > 13){
+            System.out.println("Oh dear, you're shooting far too high!");
         }
 
-
-// LOOP
+//  LOOP
 
 
     }
