2c2ee7ce85052d43f7da280b2363a4da775cc732
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-11 15:17:02 +0100

    set up folder structure

---
+++ src/main/java/SecretNumber.java
@@ -0,0 +1,5 @@
+public class SecretNumber {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/WhatToWear.java
@@ -0,0 +1,5 @@
+public class WhatToWear {
+    public static void main(String[] args) {
+
+    }
+}
