b75a55fbe1b45ff9ecf1c70453ff0321b92c3bfc
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-11 16:44:38 +0100

    range and respone for number too high

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -4,7 +4,7 @@ public class SecretNumber {
     public static void main(String[] args) {
 
 // Prompt user to guess the secret number
-        System.out.println("Can you guess the secret number?, type it in below!");
+        System.out.println("Can you guess the secret number from 1 to 20?, type it in below!");
 
 // Collect user input/Create scanner object
         Scanner reader = new Scanner(System.in);
@@ -13,8 +13,11 @@ public class SecretNumber {
 // Create response for when secret number is found
         if (Secret_Number==(13)){
             System.out.println("ELEMENTARY WATSON, YOU FOUND IT!!!");
-        }
 // Create ranges and response for wrong number
+        } else if (Secret_Number < 13){
+            System.out.println("Not quite old boy, you fell too short!");
+        }
+
 
 // LOOP
 
